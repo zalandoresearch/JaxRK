3137f8e09437648390646600ec9071896566ee72
Ingmar Schuster <ingmar.schuster@zalando.de>
2020-05-05 20:17:55 +0200

    initial frank-wolfe implementation

---
+++ jaxrk/utilities/frank_wolfe.py
@@ -0,0 +1,54 @@
+import jax.numpy as np
+from jax import grad
+from jaxrk.rkhs import FiniteVec
+from jax.random import randint, PRNGKey
+from scipy.optimize import minimize
+from time import time
+from numpy.random import randint
+
+import numpy as onp
+
+__all__ = ["frank_wolfe_unsigned_projection"]
+def __casted_output(function):
+    return lambda x: onp.asarray(function(x), dtype=np.float64)
+
+def frank_wolfe_unsigned_projection(element:FiniteVec, solution:FiniteVec = None, num_samples:np.int32 = 100):
+    assert(len(element) == 1)
+    #key = PRNGKey(np.int32(time()))
+    if solution is None:
+        solution = FiniteVec(element.k, element.inspace_points[:1, :], np.zeros(1), points_per_split=1)
+    for k in range(num_samples):
+        def cost(x):
+            x = x.reshape((1, -1))
+            return (solution(x) - element(x)).sum()
+        g_cost = grad(cost)
+        idx = randint(0, element.points_per_split - 1)
+        cand = element.inspace_points[idx:idx+1, :]
+        #print(cand)
+        #print(cost(cand), grad(cost)(cand))
+        res = minimize(__casted_output(cost), cand, jac = __casted_output(g_cost))
+        solution.inspace_points = np.vstack([solution.inspace_points, res["x"]])
+        gamma_k = 1./(k + 1)
+        solution.prefactors = np.hstack([(1-gamma_k) * solution.prefactors, gamma_k])
+        solution.points_per_split = solution.points_per_split + 1
+    return solution
+
+def frank_wolfe_fx(element:FiniteVec, num_samples:np.int32 = 100):
+    assert(len(element) == 1)
+    #key = PRNGKey(np.int32(time()))
+    solution = FiniteVec(element.k, element.inspace_points[:1, :], np.zeros(1), points_per_split=1)
+    for k in range(num_samples):
+        def cost(x):
+            x = x.reshape((1, -1))
+            return (solution(x) - element(x)).sum()
+        g_cost = grad(cost)
+        idx = randint(0, element.points_per_split - 1)
+        cand = element.inspace_points[idx:idx+1, :]
+        #print(cand)
+        #print(cost(cand), grad(cost)(cand))
+        res = minimize(__casted_output(cost), cand, jac = __casted_output(g_cost))
+        solution.inspace_points = np.vstack([solution.inspace_points, res["x"]])
+        gamma_k = 1./(k + 1)
+        solution.prefactors = np.hstack([(1-gamma_k) * solution.prefactors, gamma_k])
+        solution.points_per_split = solution.points_per_split + 1
+    return solution
