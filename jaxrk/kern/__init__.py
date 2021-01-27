3fdc194770610f3e8102d306c720d19424fd4d6f
Ingmar Schuster <ingmar.schuster@zalando.de>
2021-01-27 17:44:18 +0100

    added DictKernel

--- jaxrk/kern/__init__.py
+++ jaxrk/kern/__init__.py
@@ -1,4 +1,4 @@
 from .base import Kernel, DensityKernel, median_heuristic
-from .simple import FeatMapKernel, LinearKernel, PeriodicKernel, SpikeKernel
+from .simple import FeatMapKernel, LinearKernel, PeriodicKernel, ThreshSpikeKernel, DictKernel
 from .rbf import GenGaussKernel,  GaussianKernel, LaplaceKernel
 from .adapt_combine import SplitDimsKernel, SKlKernel
--- jaxrk/kern/simple.py
+++ jaxrk/kern/simple.py
@@ -1,9 +1,8 @@
-
+from pathlib2 import Path
 from typing import Callable
 
-import jax.numpy as np
-import jax.scipy as sp
-import jax.scipy.stats as stats
+import numpy as onp
+import jax.numpy as np, jax.scipy as sp, jax.scipy.stats as stats
 from jax.numpy import exp, log, sqrt
 from jax.scipy.special import logsumexp
 from scipy.optimize import minimize
@@ -78,9 +77,9 @@ class PeriodicKernel(Kernel):
         assert(not logsp)
         return exp(- 2* np.sin(np.pi*dists)**2 / self.ls**2)
 
-class SpikeKernel(Kernel):
+class ThreshSpikeKernel(Kernel):
     def __init__(self, spike:float = 1., non_spike:float = 0.5, threshold_sq_eucl_distance = 0.):
-        """[summary]
+        """Takes on spike falue if squared euclidean distance is below a certain threshold, else non_spike value
 
         Args:
             spike (float, optional): Kernel value > 0 when input point is equal. Defaults to 1.
@@ -101,3 +100,64 @@ class SpikeKernel(Kernel):
             dists = eucldist(X, Y, power = 2.)
         assert(not logsp)
         return np.where(dists <= self.threshold_sq_eucl_distance, self.spike, self.non_spike)
+
+class DictKernel(Kernel):
+    def __init__(self, inspace_vals:np.array, gram_values:np.array):
+        """Kernel for a fixed dictionary of input space values and accompanying gram values. Example:
+        ```
+            k = DictKernel(np.array([1,3]), np.array([(2, -1), (-1, 1.2)]))
+            assert k(np.array(1), np.array(1)) == 2
+            assert k(np.array(3), np.array(1)) == -1
+            assert k(np.array(3), np.array(3)) == 1.2
+            k(np.array(2), np.array(3)) #this will throw an exception, as 2 is not a valid inspace value
+        ```
+        Args:
+            inspace_vals: Order of input space values that this DictKernel is valid for.
+            gram_values: Gram values of shape `[len(inspace_vals), len(inspace_vals)]`.
+        """
+
+        assert len(inspace_vals.shape) == 1
+        assert gram_values.shape == (len(inspace_vals), len(inspace_vals))
+        assert np.all(gram_values == gram_values.T)
+
+        self.inspace_vals = inspace_vals
+        self.gram_values = gram_values
+    
+    @classmethod
+    def read_file(cls, p:Path):
+        with open(p) as matrix_file:
+            lines = matrix_file.readlines()
+
+        header = None
+        col_header = []
+        m = []
+        for idx, row in enumerate(lines):
+            row = row.strip()
+            if row[0] == '#' or len(row) == 0:
+                continue
+            entries = row.split()
+            if header is None:
+                header = entries
+                continue
+            else:
+                col_header.append(entries.pop(0))
+                m.append(list(map(float, entries)))
+        header, col_header = onp.array(header), onp.array(col_header)
+        m = np.array(m)
+        assert np.all(header == col_header)
+        assert len(header) == m.shape[0] and m.shape[0] == m.shape[1]
+
+        if header[-1] == '*':
+            header = header[:-1]
+            m = m[:-1, :-1]
+
+        return cls(header, m)
+
+    def gram(self, X, Y=None, diag = False, logsp = False):
+        assert(len(np.shape(X))==2)
+        idx_X = np.argmax(X == self.inspace_vals[None,:], 1)
+        idx_Y = np.argmax(Y == self.inspace_vals[None,:], 1)
+        if diag:
+            assert()
+        else:
+            return self.gram_values[idx_X,:][:, idx_Y]
