8068f9dba0ad99b86520c2eb3c997a06cc7838f1
Ingmar Schuster <ingmar.schuster@zalando.de>
2021-06-09 11:59:57 +0200

    Added softplus bijection, adapted CholeskyBijection to use it

--- jaxrk/core/constraints.py
+++ jaxrk/core/constraints.py
@@ -8,6 +8,11 @@ from ..core.typing import Bijection
 import jax.lax as lax
 
 
+def softplus(x):
+    return np.log( 1 +np.exp(x))
+
+def inv_softplus(y):
+    return np.log(np.exp(y) - 1)
 
 def squareplus(x):
     #(x + np.sqrt(x**2 + 4))/2
@@ -21,36 +26,39 @@ def squareplus_deriv(x):
     #(1 + x/np.sqrt(4 + x**2))/2
     return lax.mul(0.5, lax.add(lax.div(x, lax.sqrt(lax.add(lax.square(x), 4.))), 1.))
 
+def inv_squareplus_deriv(y):
+    pos_res = np.sqrt((-4.* y**2 + 4. * y - 1)/(y**2 - y))
+    return  np.where(y < 0.5, -pos_res, pos_res)  #2. / np.sqrt(1./np.sqrt(2*y - 2) - 1)
+
 
 
 class SquashingToBounded(Bijection):
-    def __init__(self, lower_bound:float, upper_bound:float, f = squareplus_deriv):
+    def __init__(self, lower_bound:float, upper_bound:float, f = squareplus_deriv, inv_f = inv_squareplus_deriv):
         assert lower_bound < upper_bound
         assert lower_bound is not None and upper_bound is not None
         self.lower_bound = lower_bound
         self.scale = upper_bound - lower_bound
         self.f = f
+        self.inv_f = inv_f
 
     def __call__(self, x):
         return self.scale * np.clip(self.f(x), 0., 1.) + self.lower_bound
 
     def inv(self, y):
-        raise NotImplementedError
-        return -np.log((self.upper_bound - self.lower_bound)/(y - self.lower_bound) - 1)
+        return self.inv_f((y-self.lower_bound) / self.scale)
 
 class NonnegToLowerBd(Bijection):
-    def __init__(self, lower_bound:float = 0., f = squareplus ):
+    def __init__(self, lower_bound:float = 0., f = squareplus, inv_f = inv_squareplus):
         assert lower_bound is not None
         self.lower_bound = lower_bound
         self.f = f
+        self.inv_f = inv_f
 
     def __call__(self, x):
         return np.clip(self.f(x), 0.) + self.lower_bound
     
     def inv(self, y):
-        raise NotImplementedError
-        y = y - self.lower_bound
-        return self.f_inv(y)
+        return self.inv_f(y - self.lower_bound)
 
 class FlipLowerToUpperBound(Bijection):
     def __init__(self, upper_bound:float, lb_bij:Callable[..., Bijection]):
@@ -69,17 +77,17 @@ def NonnegToUpperBd(upper_bound:float = 0.):
     
 def SoftBound(l:float = None, u:float = None) -> Bijection:
     if l is None:
-        assert u is not None, "Require one bound."
+        assert u is not None, "Requiring one bound."
         return NonnegToUpperBd(u)
     elif u is None:
-        assert l is not None, "Require one bound."
+        assert l is not None, "Requiring one bound."
         return NonnegToLowerBd(l)
     else:
         return SquashingToBounded(l, u)
 
 @dataclass
 class CholeskyBijection(Bijection):
-    diag_bij:Bijection = NonnegToLowerBd()
+    diag_bij:Bijection = NonnegToLowerBd(f=softplus, inv_f=inv_softplus)
     lower:bool = True
 
     def _standardize(self, inp):
@@ -93,11 +101,11 @@ class CholeskyBijection(Bijection):
     def param_to_chol(self, param):
         param = self._standardize(param)
 
-        return np.tril(param, -1) + self.diag_bij(np.diagonal(param))
+        return np.tril(param, -1) + np.diagflat(self.diag_bij(np.diagonal(param)))
 
     def chol_to_param(self, chol):
         chol = self._standardize(chol)
-        return np.tril(chol, -1) + self.diag_bij.inv(np.diagonal(chol))
+        return np.tril(chol, -1) + np.diagflat(self.diag_bij.inv(np.diagonal(chol)))
 
     def __call__(self, x):
         c = self.param_to_chol(x)
--- tests/test_constraints_bijections.py
+++ tests/test_constraints_bijections.py
@@ -1,5 +1,5 @@
 import jax.numpy as np
-from jaxrk.core.constraints import SoftBound
+from jaxrk.core.constraints import SoftBound, CholeskyBijection
 
 def test_simple_bijections(atol = 1e-3):
     for lb in (-0.4, 5.):
@@ -21,5 +21,10 @@ def test_simple_bijections(atol = 1e-3):
 
             assert np.all(lux > lb) and np.all(lux < ub)
             assert np.abs(lux[0] - lb) < 0.2 and np.abs(lux[-1] - ub) < 0.2
-            #for i, n in [(l.inv(lx), "lower"), (lu.inv(lux), "lower & upper"), (u.inv(ux), "upper")]:
-            #    assert np.allclose(x, i, atol = atol), f"Inverse of {n} bound Bijection is inaccurate(max abs error: {np.abs(x-i).max()}, mean abs error: {np.abs(x-i).mean()}, min abs error: {np.abs(x-i).min()})"
+            for i, n in [(l.inv(lx), "lower"), (lu.inv(lux), "lower & upper"), (u.inv(ux), "upper")]:
+                assert np.allclose(x, i, atol = atol), f"Inverse of {n} bound Bijection is inaccurate(max abs error: {np.abs(x-i).max()}, mean abs error: {np.abs(x-i).mean()}, min abs error: {np.abs(x-i).min()})"
+
+def test_cholesky_bijection():
+    cb = CholeskyBijection()
+    chol = cb.param_to_chol(np.arange(9).reshape(3, 3))
+    assert np.allclose(cb.param_to_chol(cb.chol_to_param(chol)), chol)
